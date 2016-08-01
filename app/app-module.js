9675cc6dac41f0ddee49e2b7efde4bb0fc50f0c5
Juan Jose Llado <jllavec@gmail.com>
2016-08-01 17:03:27 +0200

    First commit

---
+++ app/app-module.js
@@ -0,0 +1,16 @@
+(function () {
+  'use strict';
+
+  /* @ngdoc object
+   * @name promisesAng
+   * @description
+   *
+   */
+  angular
+    .module('promisesAng', [
+      'ngAria',
+      'ui.router',
+      'ui.bootstrap',
+      'home'
+    ]);
+}());
